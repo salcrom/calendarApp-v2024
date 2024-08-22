b25ceb03686a6939560731ba79e15a18ac7ea596
salcrom <seya.28@gmail.com>
2024-08-22 09:53:25 +0200

    autenticación completada

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,35 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+
 import { LoginPage } from "../auth";
 import { CalendarPage } from "../calendar";
+import { useAuthStore } from "../hooks";
+import { useEffect } from "react";
 
 export const AppRouter = () => {
-    const authStatus = "not-authenticated";
+    const { status, checkAuthToken } = useAuthStore();
+    // const authStatus = "not-authenticated"; // "checking", 'authenticated', "not-authenticated"
+
+    useEffect(() => {
+        checkAuthToken();
+    }, []);
+
+    if (status === "checking") {
+        return <h3>Cargando...</h3>;
+    }
 
     return (
         <Routes>
-            {authStatus === "authenticated" ? ( //'authenticated', "not-authenticated"
-                <Route path="/auth/*" element={<LoginPage />} />
+            {status === "not-authenticated" ? (
+                <>
+                    <Route path="/auth/*" element={<LoginPage />} />
+                    <Route path="/*" element={<Navigate to="/auth/login" />} />
+                </>
             ) : (
-                <Route path="/*" element={<CalendarPage />} />
+                <>
+                    <Route path="/" element={<CalendarPage />} />
+                    <Route path="/*" element={<Navigate to="/" />} />
+                </>
             )}
-
-            <Route path="/*" element={<Navigate to="/auth/login" />} />
         </Routes>
     );
 };
